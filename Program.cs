0e9d709df39eb57b6d0e6b19a40a00eacd8c17bf
Raylla-Silva12 <raylla.l.silva@gmail.com>
2021-09-03 14:12:02 -0300

    C# cod_2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace l_repeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nDesenvolva um programa que mostre na tela a tabuada do 4.\n");
+            for (int num = 0; num <= 10; num++)
+            {
+                Console.WriteLine("4 x " + num + " = " + (4 * num));
+            }
+            Console.ReadKey();
+        }
+    }
+}
